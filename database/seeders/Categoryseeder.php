824b37514af57f15113b9a0d4bffa5859af5189f
Revan610 <revanakhlf610@gmail.com>
2025-01-23 14:06:02 +0700

    commit pertama-ku

---
+++ database/seeders/Categoryseeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Categoryseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //category array
+        $categories = [];
+        for($i=1;$i<=100;$i++){
+            category::create([
+                'name' => 'Category'.$i,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}

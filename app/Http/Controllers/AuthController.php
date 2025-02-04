b4f2823e5936ac070fb273f95a429134a7ac3e6a
Reyshellby <reyshellby@gmail.com>
2025-02-04 13:40:59 +0700

    register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:250',
+            'email' => 'required|string|email|max:250|unique:users',
+            'phone_number' => 'required|string|max:250|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'Access_token'=> $token,
+            'token_type'=>'Bearer',
+            'user' => $user,
+            'message' => 'AKUN BERHASIL DIBUAT'
+        ]);
+    }
+
+    public function show(string $id)
+    {
+
+    }
+
+    public function update(Request $request, string $id)
+    {
+
+    }
+
+    public function destroy(string $id)
+    {
+
+    }
+}

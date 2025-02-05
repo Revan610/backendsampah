fa1351dbc1356e63adbf914e7f87bd904573ffc6
Reyshellby <reyshellby@gmail.com>
2025-02-05 11:53:30 +0700

    #dugam

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,8 +18,6 @@ return new class extends Migration
             $table->string('password');
             $table->string('phone_number')->unique();
             $table->enum('role', ['admin', 'user'])->default('user');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
 

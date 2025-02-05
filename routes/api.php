86a16494ecb07f299b5277ac0f1395fa3267b23b
Revan610 <revanakhlf610@gmail.com>
2025-02-05 10:18:30 +0700

    Seeder category, must migrate#3

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');

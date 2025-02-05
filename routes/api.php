89f2cc90d5e315b71fa694ba23c23f4470040ce1
Revan610 <revanakhlf610@gmail.com>
2025-02-05 10:15:40 +0700

    Seeder category, must migrate#2

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-//percobaan push pertama
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
